e2f886fcf8cbb5bf573e1baaca3636b2c2bb4c1f
fpascuales <fpascuales@gmail.com>
2023-06-10 13:57:31 +0200

    Create new getAddressStationById method

--- src/api/stations/stations.controller.js
+++ src/api/stations/stations.controller.js
@@ -74,6 +74,19 @@ const getAllStationsAdmin = async (req, res) => {
       return res.status(500).json({msg: 'Internal Server Error'});
   }
 }
+const getAddressStationById = async (req, res) => {
+  try {
+      const { id } = req.params;
+      const station = await Station.findById(id).select('address');
+      if(!station){
+          return res.status(404).json({msg: 'Not Found'});
+      }
+      return res.status(200).json(station);
+  } catch (error) {
+      //pasar el error a grafana
+      return res.status(500).json({msg: 'Internal Server Error'});
+  }
+}
 const getStationById = async (req, res) => {
     try {
         const { id } = req.params;
@@ -94,5 +107,6 @@ module.exports = {
     deleteStation,
     getAllStations,
     getAllStationsAdmin,
+    getAddressStationById,
     getStationById
 }
--- src/api/stations/stations.routes.js
+++ src/api/stations/stations.routes.js
@@ -1,7 +1,7 @@
 const { isAdmin } = require("../../middlewares/auth");
 const { verifyCreateStationFields, verifyUpdateStationFields } = require("../../middlewares/fields");
 const uploadImage = require("../../middlewares/file");
-const { createStation, updateStation, deleteStation, getAllStations, getStationById, getAllStationsAdmin } = require("./stations.controller");
+const { createStation, updateStation, deleteStation, getAllStations, getStationById, getAllStationsAdmin, getAddressStationById } = require("./stations.controller");
 
 const stationsRoutes = require("express").Router();
 
@@ -11,6 +11,7 @@ stationsRoutes.delete("/:id", [isAdmin], deleteStation);
 stationsRoutes.get("/", getAllStations);
 stationsRoutes.get("/", getAllStations);
 stationsRoutes.get("/stations-admin", getAllStationsAdmin);
+stationsRoutes.get("/station-address", getAddressStationById),
 stationsRoutes.get("/:id", getStationById);
 
 module.exports = stationsRoutes;
